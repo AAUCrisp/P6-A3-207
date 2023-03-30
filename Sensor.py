de046dabb6941836f2284d9d006673c6fff92eda
Mast3r_waf1z <thomasmollerjensen@live.dk>
2023-03-30 15:50:12 +0200

    \033[38;2;255;175;175mbug fixing

--- Sensor.py
+++ Sensor.py
@@ -34,7 +34,7 @@ class Sensor:
             # run infinitely
             while True:
                 # create a dataframe with the data "something"
-                dataFrame = ProcessData(ProcessData("n3").buildFrame())
+                dataFrame = ProcessData(ProcessData("n3").buildFrame()) 
                 # transmit the dataframe, here buildFrame is called to convert the processed data to a string
                 self.network.transmit(dataFrame.buildFrame())
                 # for loop to show a timer in the terminal showing when the next data will be transmitted
