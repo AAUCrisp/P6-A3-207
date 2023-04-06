7f4dde1253020f1403ee32c1f24a3d8adfdd8f2d
Mast3r_waf1z <thomasmollerjensen@live.dk>
2023-04-06 15:47:03 +0200

    implemented issue

--- Servertest.py
+++ Servertest.py
@@ -2,6 +2,8 @@ import include.Network as n
 from sys import argv
 from threading import Thread
 from time import sleep
+from include.Formatting import blue, red, magenta, white, black
+from include.ProcessData import EOP, SEP, DSEP
 
 ADDR = ""
 PORT= 12345 if not "--port" in argv else int(argv[argv.index("--port")+1])
@@ -11,7 +13,8 @@ Thread(target=s.listener, args=(PORT,)).start()
 while True:
     for key in s.data.keys():
         if len(s.data[key]) > 0:
-            data = s.data[key].pop(0)
-            print(f'{data["data"]} timestamp: {data["recvTime"]}')
+            for _ in range(len(s.data.keys())):
+                data = s.data[key].pop(0)
+                print(data["data"].replace(SEP, red("\t| ")).replace(DSEP, blue("\t| ")).replace(EOP, magenta("\t| ")))
         else:
             sleep(1)
