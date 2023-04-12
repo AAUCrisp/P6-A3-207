e3e10692796a54d07e86004b7061aa80c8aaea28
Mast3r_waf1z <thomasmollerjensen@live.dk>
2023-04-12 10:14:54 +0200

    changed sensor synchronization to use new implementation of clock

--- sensor.py
+++ sensor.py
@@ -4,7 +4,8 @@ from include.Network import *
 from include.ProcessData import *
 from include.setup import *
 
-from time import sleep, time
+from time import sleep
+from threading import Thread, Lock
 
 
 # This module has been documented with DocString, it is a string format following a definition, it will show up in your VSCode documentation on hovering
@@ -12,6 +13,7 @@ from time import sleep, time
 interval = 10
 """This is the interval the sensor will transmit data in"""
 
+syncLock = Lock()
 
 class Sensor:
     """This is the main class of the sensor. it will use the programs defined under `include/` to emulate the functionality of a sensor."""
@@ -26,6 +28,7 @@ class Sensor:
         self.network.connect(ipTarget, portTarget)
     
     def run(self):
+        global SVTClock, GTClock, syncLock
         """This method runs the sensor program, it will send data using the network every <interval> seconds"""
         # try/catch clause to restore terminal state after a keyboardinterrupt
         try: 
@@ -35,27 +38,48 @@ class Sensor:
             txTime = -1
             postTxTime = -1
             while True:
-                sleepEnd = time()+interval
-                while sleepEnd > time():
-                    print(int(sleepEnd-time()), end="\r")
+                syncLock.acquire()
+                sleepEnd = SVTClock.get()+interval
+                while sleepEnd > SVTClock.get():
+                    print(int(sleepEnd-SVTClock.get()), end="\r")
                     sleep(1)
-                    if (sleepEnd - time()) < 5 and (sleepEnd - time()) > 4:
-                        dataTime = time()
+                    if (sleepEnd - SVTClock.get()) < 5 and (sleepEnd - SVTClock.get()) > 4:
+                        dataTime = SVTClock.get()
+                        payload = "some payload"
+                syncLock.release()
                 dataframe = ProcessData()
 
                 dataframe.setDataTime(dataTime)
                 dataframe.setTxTime(txTime)
                 dataframe.setPostTxTime(postTxTime)
-                dataframe.setPayload("some payload")
+                dataframe.setPayload(payload)
 
-                txTime = time()
+                txTime = SVTClock.get()
                 self.network.transmit(dataframe.buildSensorFrame())
-                postTxTime = time()
+                postTxTime = SVTClock.get()
 
         except KeyboardInterrupt: unhide()
 
+def runSync():
+    global SVTClock, GTClock, syncLock
+    s = Sync(
+        addressGT=  ips["up2"]["ethernet"],
+        address=    ips["up2"]["wifi"],
+        interfaceGT="ethernet",
+        interface=  "wifi"
+    )
+    while True:
+        syncLock.acquire()
+        GTClock.set(s.syncGT())
+        SVTClock.set(s.sync())
+        syncLock.release()
+        sleep(30) # only sync every 30 seconds
+
 # The main of this program
 if "main" in __name__:
+    # start a synchronizing thread
+    syncThread =  Thread(target=runSync, daemon=True)
+    syncThread.start()
 
     # create a sensor object with the headend address as an argument
     sensor = Sensor((ipTarget, portTarget), interfaceTarget)
