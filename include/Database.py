d64908afe262ef06cb8d16cd66a8d7e0d6ee7003
AAUCrisp <stpe19@student.aau.dk>
2023-04-17 16:54:07 +0200

    Getting closer with the insertion of HeadendTransfer data :unicorn:

--- include/Database.py
+++ include/Database.py
@@ -36,73 +36,68 @@ def unpack(packet, recvIP, recvTime):
 
             print(f"\nHeadend Frame Number:       {i+1}\n")
             # print(f" -  Current Frama Data:         {key}")
+            print(f" -  Current Receive IP:         {lastIP}")
             print(f" -  Current rxTime:             {frameData[0]}")
             print(f" -  Current txTime:             {frameData[1]}")
+            nodeData[i]['nodeIP'] = lastIP
             nodeData[i]['rxTime'] = frameData[0]
             nodeData[i]['txTime'] = frameData[1]
+            # print(f" -  Current Payload:            {frameData[4]}")
+            lastIP = frameData[3]
 
             # pigLen = frameData[2].count(DSEP)
             # print(f"Piggy Frame: {pigLen}")
 
             if frameData[2].count(DSEP) > 0:
                 pigFrame = frameData[2].split(DSEP)
-                print(f" -  Current prevTxTime:         {pigFrame[0]}")
+                print(f" -  Current postTxTime:         {pigFrame[0]}")
                 print(f" -  Current Piggy:              {pigFrame[1]}")
-                nodeData[i]['prevTxTime'] = pigFrame[0]
-                nodeData[i]['piggy'] = pigFrame[1]
+                nodeData[i]['postTxTime'] = pigFrame[0]
+                nodeData[i]['payload'] = pigFrame[1]
+                # nodeData[i]['piggy'] = pigFrame[1]
 
                 ###  OH SHIT!!! Need to add Piggy-data as a Combined !!
 
             else:
-                print(f" -  Current prevTxTime:         {frameData[2]}")
-                nodeData[i]['prevTxTime'] = frameData[2]
+                print(f" -  Current postTxTime:         {frameData[2]}")
+                nodeData[i]['postTxTime'] = frameData[2]
                 
-            print(f" -  Current Receive IP:         {lastIP}")
-            nodeData[i]['recvIP'] = frameData[3]
-            # print(f" -  Current Payload:            {frameData[4]}")
-            lastIP = frameData[3]
 
 
 
     frameData = nodes[layers-1].split(SEP)
 
     print(f"\nSensor Frame Number:        {layers}")
+    print(f" -  Current Receive IP:         {lastIP}")
     print(f" -  Sensor genTime:             {frameData[0]}")
     print(f" -  Sensor txTime:              {frameData[1]}")
-    print(f" -  Sensor prevTxTime:          {frameData[2]}")
+    print(f" -  Sensor postTxTime:          {frameData[2]}")
     print(f" -  Sensor Payload:             {frameData[3]}")
+    nodeData[layers-1]['nodeIP'] = lastIP
     nodeData[layers-1]['rxTime'] = frameData[0]
     nodeData[layers-1]['txTime'] = frameData[1]
-    nodeData[layers-1]['prevTxTime'] = frameData[2]
-    nodeData[layers-1]['recvIP'] = lastIP
+    nodeData[layers-1]['postTxTime'] = frameData[2]
     nodeData[layers-1]['payload'] = frameData[3]
 
     comDelay = float(recvTime) - float(frameData[0])
 
-    sensorParams = { 
-        'where': {
-            'ip5g': lastIP,
-            'OR': None,
-            'ipWifi': lastIP
-            }, 
-        }
 
-
-    print(f"\nNodeData Dict Contains:")
-    db.insertData(nodeData)
-
-    # for i in range(layers):
+    # print(f"\nNodeData Dict Contains:")
+    # for i in range(len(nodeData)):
     #     print(nodeData[i])
 
-    print(f"\nSensor fetch parameters in Backend is: {sensorParams['where']}\n")
 
-    sensorData = db.fetch('Node', sensorParams)
+    db.insertData(nodeData)
 
-    print(f"Fetched Node Data is: {sensorData}")
+
+
+    sensorData = db.fetchSensorInfo(lastIP)
+
+    print(f"Fetched Sensor Data is: {sensorData}")
 
     # Needs updates for the GT data...
     comTrans = { 
-        'sensorId': sensorData[0]['id'],
+        'sensorId': sensorData[0]['sensorId'],
         'combinedDelay': comDelay,
         'combinedDelayGT': comDelay,
         'dataTime': frameData[0],
@@ -113,6 +108,7 @@ def unpack(packet, recvIP, recvTime):
         }
 
     db.insert('CombinedTransfer', comTrans)
+
 
 
 #  Remove after figuring shit out!!!
@@ -149,7 +145,36 @@ class Database():
 
     ######################################
     #  --  General fetch function  --
+
     def fetch(self, table, setup = None):
+        """General Fetch Function
+        -
+        Will by default return everything in the requested table, as is configurable to whatever needed.
+
+        
+        Args:
+        -----
+        - `table (str)`: The table to get data from.
+        - `setup (dict, optional)`: Configuration to specify the needs of your query.
+
+        ________  
+
+        Setup Options:
+        -----
+        Left ones are keys in the dict, right is values  
+
+        - `select (str)`:  "column, other_column"  Really shouldn't be this way.
+        - `where (dict)`:  {column: value}
+        - `join (dict)`:  {join_table: 'OG_table.OG_column = join_table.join_column'}.  
+        - `limit (int)`:  Limit number of returned rows.  
+        - `order (dict)`:  {'column: 'ASC|DESC'}.  
+        
+        ________  
+        
+        Returns:
+        -----
+        `dict`: containing the retrieved data
+        """        
         # print(setup)
         # print("Inside DB Fetch")
 
@@ -162,7 +187,10 @@ class Database():
         # Default fetch config
         config = {'select': '*', 
             'where': None,
-            'join': None}
+            'join': None,
+            'limit': None,
+            'order': None
+            }
 
         # Replace defaults with set settings, if any exists
         if setup:
@@ -208,8 +236,19 @@ class Database():
                 if key != "OR":
                     sql += f"""{key}='{config['where'][key]}'"""
 
+        if config['order']:
+            sql += f"""
+            ORDER BY """
+            for i, key in enumerate(config['order']):
+                if i > 0:
+                    sql += ", "
+                sql += f"{key} {config['order'][key]}"
+        if config['limit']:
+            sql += f"""
+            LIMIT {config['limit']}"""
 
-        # print(f"\nSQL Statement is:{sql}\n\n")
+
+        print(f"\nSQL Statement is:{sql}\n\n")
         self.cur.execute(sql)
         # print("Efter Execute")
         rows = self.cur.fetchall()
@@ -217,8 +256,8 @@ class Database():
         # rows = self.cur.fetchone()
 
 
-    ####################################
-    ###  --  Test Printing Area  -- 
+        ####################################
+        ###  --  Test Printing Area  -- 
         # print(f"Fetched data is:")
         # for row in rows:
         #     print(f"{row}")
@@ -227,6 +266,42 @@ class Database():
 
 
 
+    def fetchSensorInfo(self, nodeIP):
+        """Fetches the needed info for inserting the new sensor data.
+
+        ______  
+
+        Args:
+        ---
+            `nodeIP (str)`: IP address for the origin-sensor
+
+        ______  
+
+        Returns:
+        ---
+            `dict`: with `sensorId` and `txId` keys
+        """    
+
+        sensorParams = { 
+            'select': "CombinedTransfer.sensorId, CombinedTransfer.id AS txId",
+            'where': {
+                'ip5g': nodeIP,
+                'OR': None,
+                'ipWifi': nodeIP
+                },
+            'join':{
+                'CombinedTransfer': 'Node.id=CombinedTransfer.sensorId'
+                },
+            'order': {
+                'CombinedTransfer.id': 'DESC'
+                },
+            'limit': 1
+            }
+
+
+        sensorData = db.fetch('Node', sensorParams)
+
+        return sensorData
 
 
 ##########################################
@@ -279,33 +354,100 @@ class Database():
         
         # print(nodeData)
 
-        piggyFrames = []
+        payloadFrames = []
+        oldDelays = []
+        newInput = []
+        sensorData = []
+        oldHeadends = []
+
+
+        # print("Full NodeData is:")
+        # for i in range(len(nodeData)):
+        #     print(nodeData[i])
+
 
         for i in range(len(nodeData)):
-            print(nodeData[i])
 
-            # Update Last Transmissions tx and prevTx data
+            # Update Last Transmissions tx and postTx data
             # - Fetch newest CompleteTransfer from this SensorIP
             # -- Then fetch the HeadendTransfer IDs attached to it
-            # --- Update (insert) tx and prevTx values in "old data"
+            # --- Update (insert) tx and postTx values in "old data"
             # ---- Can likely do it in a single SQL Sentence
+
+            oldDelays.append({
+                'nodeIP': nodeData[i]['nodeIP'],
+                'txTime': nodeData[i]['txTime'],
+                'postTxTime': nodeData[i]['postTxTime']
+            })
+
+            newInput.append({
+                'nodeIP': nodeData[i]['nodeIP'],
+                'rxTime': nodeData[i]['rxTime']
+            })
 
 
             if 'payload' in nodeData[i]:
                 # THIS IS SENSOR DATA!!!
 
                 # Insert in CombinedTransfer Table
-                print("Sensor Frame State, around line 300 in DB")
+                print("Sensor Frame State, around line 400 in DB")
 
-            elif 'piggy' in nodeData[i]:
-                print("Sensor Frame State, around line 300 in DB")
-                piggyFrames.append(i)
+                newInput[i].__setitem__('payload', nodeData[i]['payload'])
+
+                sensorData.append(db.fetchSensorInfo(nodeData[i]['nodeIP'])[0])
+
+
+                payloadFrames.append(i)
+
 
             else:
                 # Insert in HeadendTransfer Table
-                print("Headend Frame State, around line 300 in DB")
+                print("Headend Frame State, around line 400 in DB")
 
-        print(piggyFrames)
+            # print(nodeData[i])
+
+
+
+        # print(f"Old Frame Data is:")
+        # for i in range(len(oldHeadends)):
+        #     print(oldHeadends)
+
+
+        # print(f"\nSensorData is: {sensorData[1][0]['sensorId']}")
+        # print(f"\nSensorData is: {sensorData}")
+
+        for j, frameKey in enumerate(payloadFrames):
+
+            print(f"\nJ is: {j}\nframeKey is: {frameKey}\n")
+
+            oldParams = {
+                'where': {
+                    'transferId': sensorData[j]['txId']
+                },
+                'order': {
+                    'id': 'DESC'
+                }
+            }
+
+            oldHeadends = db.fetch('HeadendTransfer', oldParams)
+
+
+            for i, frame in enumerate(newInput):
+
+                print(f"Index number {i} has framedata {frame}")
+                # updateParams = {
+                #     'values': {
+                #         'nodeId': oldHeadends[frameKey]['nodeId'],
+                #         'txTime': frame['txTime'],
+                #         'postTxTime': frame['postTxTime'],
+                #     }
+                # }
+
+
+
+        print(f"\nData is in Frames: {payloadFrames}")
+        print(f"Old Delays Data is:\n{oldDelays}\n")
+
 
         return 1
 
