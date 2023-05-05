b57fb755fcbaec064d78a681c379c72c59fbf112
Mast3r_waf1z <thomasmollerjensen@live.dk>
2023-05-05 22:42:31 +0200

    fixed ip addresses and logging some additional shit :skull

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-sudo ip route flush default device enp3s0
+mkdir -p testdata
 
 for file in $(ls tests); do
-./scripts/runner.sh --verbose --config=tests/"$file" --dbname="$(basename "$file" .json)"
-sleep 120
+    ./scripts/runner.sh --verbose --config=tests/"$file" --dbname="$(basename "$file" .json)"
+    for node in "192.168.1.61" "192.168.1.80" "192.168.1.107" "192.168.1.251" ; do
+        #sshpass -p 123 ssh root@"$node" ip route flush dev enp3s0
+        mkdir -p testdata/"$(basename "$file" .json)"
+        sshpass -p 123 ssh root@"$node" cat /tmp/P6-A3-207/include/db.db3 > testdata/"$(basename "$file" .json)"/db.db3
+        sshpass -p 123 ssh root@"$node" cat /tmp/P6-A3-207/screenlog.0 > testdata/"$(basename "$file" .json)"/"$node".log
+    done
+    sleep 120
 done
