c0ecd3acb37208dc59576c2d15e6256b5df6e174
Mast3r_waf1z <thomasmollerjensen@live.dk>
2023-05-07 18:56:29 +0200

    fixed downloading issue

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,8 +7,9 @@ for file in $(ls tests); do
     for node in "192.168.1.61" "192.168.1.80" "192.168.1.107" "192.168.1.251" ; do
         #sshpass -p 123 ssh root@"$node" ip route flush dev enp3s0
         mkdir -p testdata/"$(basename "$file" .json)"
-        sshpass -p 123 ssh root@"$node" cat /tmp/P6-A3-207/include/db.db3 > testdata/"$(basename "$file" .json)"/db.db3
-        sshpass -p 123 ssh root@"$node" cat /tmp/P6-A3-207/screenlog.0 > testdata/"$(basename "$file" .json)"/"$node".log
+
+        sshpass -p 123 sftp root@"$node":/tmp/P6-A3-207/include/db.db3 testdata/"$(basename "$file" .json)"/db.db3
+        sshpass -p 123 sftp root@"$node":/tmp/P6-A3-207/screenlog.0 testdata/"$(basename "$file" .json)"/"$node".log
     done
     sleep 120
 done
