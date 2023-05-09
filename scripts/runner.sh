fd43dbcc22b01c605f52c3c079825239ca9e44bc
Mast3r_waf1z <thomasmollerjensen@live.dk>
2023-05-09 16:17:31 +0200

    :skull: aaaaaaaaaaa

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -107,7 +107,7 @@ function runCondition(){
         'limit')
             datarate="$(echo "$condition" | jq -r -c .value)"
             interface="$(echo "$condition" | jq -r -c .interface)"
-            echo "screen -L -dmS limit 'echo $password | sudo -S /tmp/P6-A3-207/scripts/conditions.sh limit --datarate=$datarate --iface=$interface'" | sshpass -p "$password" ssh "$name@$ip" 'bash -s'
+            echo "screen -L -dmS limit bash -s 'echo $password | sudo -S /tmp/P6-A3-207/scripts/conditions.sh limit --datarate=$datarate --iface=$interface'" | sshpass -p "$password" ssh "$name@$ip" 'bash -s'
             ;;
     esac
 }
