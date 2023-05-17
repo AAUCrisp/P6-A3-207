d4e37d17ee47f8715583ed661cd76af19fdb86c8
AAUCrisp <stpe19@student.aau.dk>
2023-05-17 11:58:12 +0200

    Made the plot data extractor ready for data from 3 different database files... need to manually write the paths in the script right now, though :unicorn:

--- plotExtractions.py
+++ plotExtractions.py
@@ -6,15 +6,25 @@ from numpy import mean
 
 if __name__ == "__main__":
     
-    dbPath = str(os.getcwd()) + "/include/db.db3"
-    db = Database(dbPath)
+    t1DbPath = str(os.getcwd()) + "/data/syncInterval_3/database.db3"
+    t1Db = Database(t1DbPath)
+    t1S1data = t1Db.fetchPlotDelays(2)
+    t1S2data = t1Db.fetchPlotDelays(4)
 
-    # S1data = db.fetchPlotDelays(6)
-    S1data = db.fetchPlotDelays(2)
-    S2data = db.fetchPlotDelays(4)
+
+    t2DbPath = str(os.getcwd()) + "/data/syncInterval_15/database.db3"
+    t2Db = Database(t2DbPath)
+    t2S1data = t2Db.fetchPlotDelays(2)
+    t2S2data = t2Db.fetchPlotDelays(4)
+
+
+    t3DbPath = str(os.getcwd()) + "/data/syncInterval_30/database.db3"
+    t3Db = Database(t3DbPath)
+    t3S1data = t3Db.fetchPlotDelays(2)
+    t3S2data = t3Db.fetchPlotDelays(4)
 
     print(f"The extracted data is:\n")
-    for i in enumerate(S1data):
+    for i in enumerate(t1S1data):
         print(i)
 
 
