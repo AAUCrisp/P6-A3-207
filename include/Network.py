ec60438551066762a546f3dc7b645f160745d9f5
Mast3r_waf1z <thomasmollerjensen@live.dk>
2023-06-09 14:03:15 +0200

    attempt #1

--- include/Network.py
+++ include/Network.py
@@ -66,7 +66,7 @@ class Network():
                     #self.lock.acquire()                                            # Lock the following code, such that only one thread can access it. 
                     self.data.put({"recvTime":recvTime, "data":frame.split(EOT)[0], "id":threadID})        # Write the received data from the thread to a variable shared by all the threads in this process. 
                     #self.lock.release()                                                         # Release the lock once the task above is finished. 
-                    frame = frame.split(EOT)[1]
+                    frame = frame.split(EOT, 1)[1]
                 else:
                     frame += sensorData
             else:
